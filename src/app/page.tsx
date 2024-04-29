e504b78496cf2a7b33b053009077b1eb311ac297
toky-nomena <tolotra.trading@gmail.com>
2024-04-29 12:53:17 -0400

    update styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import {
   isNextSunday,
   getName,
   isCurrentMonth,
-  dayFormatter,
 } from "~/utils/date";
 
 export default function HomePage() {
@@ -25,14 +24,14 @@ function MonthView({ entry }: { entry: Entry }) {
   return (
     <div
       className={cn(
-        "bg-tile-color flex flex-col opacity-40 drop-shadow-sm",
+        "bg-tile-color flex flex-col rounded-lg opacity-40 drop-shadow-sm",
         isCurrentMonth(entry.month) && "scale-102 opacity-100",
       )}
     >
-      <div className="border-b-2 p-3 text-xl font-semibold uppercase">
+      <div className="border-background-color border-0 border-b-2 border-solid p-3 text-xl font-semibold uppercase">
         {frenchMonthNames[entry.month - 1]}
       </div>
-      <ul className="flex list-disc flex-col gap-2 p-3">
+      <ul className="flex list-disc flex-col gap-2 py-3">
         {entry.data.map((data) => (
           <DateView
             date={data.sunday}
@@ -48,17 +47,17 @@ function MonthView({ entry }: { entry: Entry }) {
 function DateView({ index, date }: { index: number; date: Date }) {
   const isNext = isNextSunday(date);
   return (
-    <li className="flex list-none items-center justify-between text-gray-400">
-      <div
-        className={cn(
-          "flex items-center gap-2 text-sm font-normal",
-          isNext && "text-md font-bold text-blue-600",
-        )}
-      >
+    <li
+      className={cn(
+        "flex list-none items-center justify-between px-3 text-gray-400",
+        isNext && "bg-green-200 py-2 text-lg text-black",
+      )}
+    >
+      <div className={"flex items-center gap-2 font-normal"}>
         <div
           className={cn(
-            "flex h-7 w-7 items-center justify-center rounded-full border-2 border-gray-400 text-xs font-semibold",
-            isNext && "border-none bg-blue-600 text-white",
+            "flex h-8 w-8 items-center justify-center rounded-full border border-gray-400 text-xs font-semibold",
+            isNext && "border-black",
           )}
         >
           {formatter.format(date)}
@@ -76,7 +75,6 @@ function DateView({ index, date }: { index: number; date: Date }) {
           stroke-width="1"
           stroke-linecap="round"
           stroke-linejoin="round"
-          className="text-blue-600"
         >
           <title>Piano</title>
           <path d="M18.5 8c-1.4 0-2.6-.8-3.2-2A6.87 6.87 0 0 0 2 9v11a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-8.5C22 9.6 20.4 8 18.5 8" />
