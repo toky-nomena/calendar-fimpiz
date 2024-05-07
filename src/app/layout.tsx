8fea007fbd9ff10a2ef96fe2817484316d3e0449
toky-nomena <tolotra.trading@gmail.com>
2024-05-06 21:21:08 -0400

    Provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
+import { Providers } from "~/providers";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -21,14 +22,16 @@ export default function RootLayout({
   return (
     <html lang="en" className="bg-background-color text-white backdrop-blur">
       <body className={`font-sans ${inter.variable}`}>
-        <header className="border-border/40 bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 px-6 py-4 backdrop-blur">
-          <span className="text-xl font-black uppercase">
-            Calendrier FIMPIZ {new Date().getFullYear()}
-          </span>
-        </header>
-        <section>
-          <div className="mx-auto font-sans">{children}</div>
-        </section>
+        <Providers>
+          <header className="border-border/40 bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 px-6 py-4 backdrop-blur">
+            <span className="text-xl font-black uppercase">
+              Calendrier FIMPIZ {new Date().getFullYear()}
+            </span>
+          </header>
+          <section>
+            <div className="mx-auto font-sans">{children}</div>
+          </section>
+        </Providers>
       </body>
     </html>
   );
